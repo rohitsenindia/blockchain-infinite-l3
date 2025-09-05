02ca2fe5e403dd885f891d8b1ebf5712e5818243
rohitsenindia <rohitsen0282@gmail.com>
2025-09-05 22:16:22 +0530

    feat: Add Layer 1 interaction functionality using Web3.py

--- modules/l3_feature_2025_09_05/feat_a_function_to_manage_interoperability_with_a_specific_Layer_1_protocol.py
+++ modules/l3_feature_2025_09_05/feat_a_function_to_manage_interoperability_with_a_specific_Layer_1_protocol.py
@@ -1,19 +1,30 @@
-import requests
+from web3 import Web3
 
-def interact_with_solana(rpc_url, action, params={}):
-    headers = {'Content-Type': 'application/json'}
-    payload = {'jsonrpc': '2.0', 'id': 1, 'method': action, 'params': params}
-    response = requests.post(rpc_url, json=payload, headers=headers)
-    response.raise_for_status()
-    return response.json()['result']
+class Layer1Interop:
+    def __init__(self, provider_url):
+        self.w3 = Web3(Web3.HTTPProvider(provider_url))
 
-def get_solana_balance(rpc_url, address):
-    return interact_with_solana(rpc_url, 'getBalance', {'commitment': 'finalized', 'account': address})
+    def get_block_number(self):
+        return self.w3.eth.blockNumber
 
-def send_solana_transaction(rpc_url, transaction):
-    return interact_with_solana(rpc_url, 'sendTransaction', {'transaction': transaction})
+    def get_balance(self, address):
+        return self.w3.eth.getBalance(address)
 
-def get_solana_block_height(rpc_url):
-    return interact_with_solana(rpc_url, 'getBlockHeight')
+    def send_transaction(self, private_key, to_address, amount, nonce=None):
+        account = self.w3.eth.account.privateKeyToAccount(private_key)
+        tx = {
+            'nonce': nonce or self.w3.eth.getTransactionCount(account.address),
+            'to': to_address,
+            'value': amount,
+            'gas': 21000,
+            'gasPrice': self.w3.eth.gasPrice
+        }
+        signed_tx = self.w3.eth.account.signTransaction(tx, private_key)
+        tx_hash = self.w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        return tx_hash.hex()
 
+    def get_transaction_receipt(self, tx_hash):
+        return self.w3.eth.getTransactionReceipt(tx_hash)
 
+    def check_connection(self):
+        return self.w3.isConnected()
