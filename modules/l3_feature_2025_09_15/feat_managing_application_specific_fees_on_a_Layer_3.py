d16a49a9248c90df2b2e179e84fb7863f45ab999
rohitsenindia <rohitsen0282@gmail.com>
2025-09-15 04:46:05 +0530

    feat: Add functions to interact with fee contract

--- modules/l3_feature_2025_09_15/feat_managing_application_specific_fees_on_a_Layer_3.py
+++ modules/l3_feature_2025_09_15/feat_managing_application_specific_fees_on_a_Layer_3.py
@@ -1,23 +1,40 @@
-from eth_typing import Address
+from eth_account import Account
 from web3 import Web3
 
-class FeeManager:
-    def __init__(self, w3: Web3, contract_address: Address):
-        self.w3 = w3
-        self.contract = self.w3.eth.contract(address=contract_address, abi=[
-            {'inputs': [{'internalType': 'uint256', 'name': '_feeAmount', 'type': 'uint256'}], 'name': 'setFee', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'},
-            {'inputs': [], 'name': 'getFee', 'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}], 'stateMutability': 'view', 'type': 'function'}
-        ])
+w3 = Web3(Web3.HTTPProvider("http://localhost:8545")) # Replace with your L3 provider
 
-    def set_fee(self, fee_amount: int, private_key: str):
-        txn = self.contract.functions.setFee(fee_amount).buildTransaction({
-            'nonce': self.w3.eth.getTransactionCount(self.w3.eth.accounts[0]),
-            'gas': 1000000,
-            'gasPrice': self.w3.eth.gas_price
-        })
-        signed_txn = self.w3.eth.account.signTransaction(txn, private_key=private_key)
-        txn_hash = self.w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-        return self.w3.eth.waitForTransactionReceipt(txn_hash)
+contract_abi = [
+    {"inputs": [], "name": "getFee", "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}], "stateMutability": "view", "type": "function"},
+    {"inputs": [{"internalType": "uint256", "name": "_fee", "type": "uint256"}], "name": "setFee", "outputs": [], "stateMutability": "nonpayable", "type": "function"},
+    {"inputs": [], "payable": True, "name": "payFee", "outputs": [], "stateMutability": "payable", "type": "function"}
+]
 
-    def get_fee(self) -> int:
-        return self.contract.functions.getFee().call()
+contract_address = "0xYourContractAddress" # Replace with your deployed contract address
+
+contract = w3.eth.contract(address=contract_address, abi=contract_address)
+
+def get_fee():
+    return contract.functions.getFee().call()
+
+def set_fee(new_fee):
+    txn = contract.functions.setFee(new_fee).buildTransaction({
+        'chainId': 1337, # Replace with your chain ID
+        'gas': 1000000,
+        'gasPrice': w3.toWei('1', 'gwei'),
+        'nonce': w3.eth.getTransactionCount("0xYourAccountAddress") # Replace with your account address
+    })
+    signed_txn = w3.eth.account.signTransaction(txn, private_key="YourPrivateKey") # Replace with your private key
+    tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    return tx_hash.hex()
+
+def pay_fee(amount):
+    txn = contract.functions.payFee().buildTransaction({
+        'chainId': 1337, # Replace with your chain ID
+        'gas': 1000000,
+        'gasPrice': w3.toWei('1', 'gwei'),
+        'value': amount,
+        'nonce': w3.eth.getTransactionCount("0xYourAccountAddress") # Replace with your account address
+    })
+    signed_txn = w3.eth.account.signTransaction(txn, private_key="YourPrivateKey") #Replace with your private key
+    tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    return tx_hash.hex()
