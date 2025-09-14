c24d907e86b7f2c4c087e08409476fd3636740d8
rohitsenindia <rohitsen0282@gmail.com>
2025-09-15 00:48:59 +0530

    feat: Implement websocket-based state synchronization between L2 and L3

---
+++ modules/l3_feature_2025_09_15/feat_a_Python_script_to_handle_state_synchronization_between_a_Layer_2_and_Layer_3.py
@@ -0,0 +1,21 @@
+import asyncio
+import websockets
+
+async def synchronize_state(l2_url, l3_url):
+    async with websockets.connect(l2_url) as l2_ws, websockets.connect(l3_url) as l3_ws:
+        while True:
+            l2_state = await l2_ws.recv()
+            await l3_ws.send(l2_state)
+            l3_state = await l3_ws.recv()
+            if l2_state != l3_state:
+                await l2_ws.send(l3_state)
+
+
+async def main():
+    l2_websocket_url = "ws://localhost:8080/l2"
+    l3_websocket_url = "ws://localhost:8081/l3"
+    await synchronize_state(l2_websocket_url, l3_websocket_url)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
